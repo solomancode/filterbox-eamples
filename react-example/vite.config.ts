d92b80b64d3c58fa38abc07a3ba4733c2a78d0a5
Mohamed Adel <solomancode@gmail.com>
2024-06-07 17:16:32 +0300

    added base url

--- react-example/vite.config.ts
+++ react-example/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-console.log(process.cwd())
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
