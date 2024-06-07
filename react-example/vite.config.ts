ea3964f13eaa9c38cea868691477e6a65e43fef0
Mohamed Adel <solomancode@gmail.com>
2024-06-07 16:11:56 +0300

    re-build demo

--- react-example/vite.config.ts
+++ react-example/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+console.log(process.cwd())
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: [
+    "src/**.css"
+  ],
+  build: {
+    outDir: "../docs/"
+  }
 })
